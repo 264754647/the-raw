6d56b0c110f586cbeefdce9b50bd204d9e42c820
264754647 <pennueng.sk143leng@gmail.com>
2026-05-11 22:23:35 +0700

    server.js

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://theraw.vercel.app",
+  credentials: true
+}));
 // Increase payload size limit for image uploads
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({
-  origin: "https://theraw-6831346221-4322-6831346221-4322s-projects.vercel.app/perfumery",
-  credentials: true
-}));
+
 
 // Request logging middleware
 app.use((req, res, next) => {
