a647d5e018316dc157d7bc61b7b6c884db0d5f6a
264754647 <pennueng.sk143leng@gmail.com>
2026-05-11 20:43:44 +0700

    trying to connect front to back

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(cors());
 // Increase payload size limit for image uploads
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors({
+  origin: "https://theraw-6831346221-4322-6831346221-4322s-projects.vercel.app/perfumery",
+  credentials: true
+}));
 
 // Request logging middleware
 app.use((req, res, next) => {
